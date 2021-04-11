35cf302953a85cb7d130db843c589409f213efc0
Tuomas Koukkari <tuomas.koukkari@helsinki.fi>
2021-04-12 00:33:57 +0300

    schema.sql päivitetty

--- schema.sql
+++ schema.sql
@@ -1,1 +1,14 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT, role TEXT);
+CREATE TABLE substances(id SERIAL PRIMARY KEY, name TEXT, target TEXT, mechanism TEXT, metabolism TEXT, eff_duration TEXT, notes TEXT, risks TEXT);
+CREATE TABLE interactions(id SERIAL PRIMARY KEY, description TEXT)
+CREATE TABLE substanceInteraction
+(
+substance_id INTEGER,
+interaction_id INTEGER,
+CONSTRAINT fk_substance
+FOREIGN KEY(substance_id)
+REFERENCES substances(id),
+CONSTRAINT fk_interaction
+FOREIGN KEY(interaction_id)
+REFERENCES interactions(id)
+);
